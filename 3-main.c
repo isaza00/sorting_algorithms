355c8950deaae9df5129b63129c5aec7f3cab7c4
Norman <isaza00@hotmail.com>
2020-06-14 16:21:51 -0500

    all

--- 3-main.c
+++ 3-main.c
@@ -13,9 +13,8 @@ int main(void)
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
-	printf("\n");
+	printf("-------------\n");
+
 	quick_sort(array, n);
-	printf("\n");
-	print_array(array, n);
 	return (0);
 }
---
+++ 3-quick_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * partition - separate the array into to partitions
+ * @array: doble pointer to node list
+ * @size: size of the array
+ * Return: prints array each time it swaps
+ */
+size_t partition(int *array, size_t first, size_t end, size_t size)
+{
+	size_t pivot_index = first, i = first;
+	int pivot = array[end], temp;
+
+	while(i < end)
+	{
+		if (array[i] < pivot)
+		{
+			if (pivot_index != i)
+			{
+				temp = array[i];
+				array[i] = array[pivot_index];
+				array[pivot_index] = temp;
+				print_array(array, size);
+			}
+			pivot_index += 1;
+			
+			
+
+		}
+		i += 1;
+	}
+	if (pivot_index != end)
+	{
+		temp = array[pivot_index];
+		array[pivot_index] = pivot;
+		array[end] = temp;
+		print_array(array, size);
+	}
+	return (pivot_index);
+}
+/**
+ * quick_sort - orders array with quick sort algo
+ * @array: doble pointer to node list
+ * @size: size of the array
+ * Return: prints array each time it swaps
+ */
+void sort(int *array, long long int first, long long int end, size_t size)
+{
+	long long int pivot_index;
+
+	if (first < end)
+	{
+		pivot_index = partition(array, first, end, size);
+		
+		sort(array, first, pivot_index - 1, size);
+		sort(array, pivot_index + 1, end, size);
+	}
+}
+/**
+ * quick_sort - orders array with quick sort algo
+ * @array: doble pointer to node list
+ * @size: size of the array
+ * Return: prints array each time it swaps
+ */
+void quick_sort(int *array, size_t size)
+{
+	sort(array, 0, size - 1, size);
+}
